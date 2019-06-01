c0018cc86aa9aaea4628d40e660970ee38bb3873
victorsouza02 <victorsouzaleal@gmail.com>
2019-06-01 12:17:14 -0300

    First Release - Working

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+loadIds();
+
+document.querySelector("#create").addEventListener("click", (e)=>{
+    saveIds();
+    createCodes();
+    document.querySelector("#create").innerHTML = "Created - Click to create again";
+
+});
+
+
+
+
+function createCodes(){
+        let date = new Date;
+
+        // CLEAR QR CODE
+        for (let index = 1; index <= 4; index++) {
+            document.querySelector("#qrcode"+index).innerHTML= "";
+        }
+
+        // MAKE NEW QR CODE
+        for (let index = 1; index <= 4; index++) {
+            if (document.querySelector("#id"+index).value != ""){
+                var qrcode = new QRCode("qrcode"+index, {
+                    text: document.querySelector("#id"+index).value +"." + date.getTime(),
+                    width: 256,
+                    height: 256,
+                    colorDark: "black",
+                    colorLight: "white",
+                    correctLevel : QRCode.CorrectLevel.H
+                });
+            } 
+        }
+}
+
+function saveIds() {
+    for (let index = 1; index <= 4; index++) {
+        localStorage.setItem("id"+index, document.querySelector("#id"+index).value);
+    }
+}
+
+function loadIds() {
+    for (let index = 1; index <= 4; index++) {
+        document.querySelector("#id"+index).value = localStorage.getItem("id"+index);
+    }
+}
